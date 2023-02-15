7909c358b616d15052e0342df5e1ec2245a971b8
Juliet-Olieng <anyangojolieng@gmail.com>
2023-02-15 13:21:45 +0300

    assignment

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,8 +7,6 @@ fun main(){
  var y=sentence("Juliet",23)
     println(y)
 
-    val length=(20)
-    println("The Class Door is:" + length.toString())
 
     val name1 = "Jane"
     val name2 = "Jane"
@@ -18,8 +16,17 @@ fun main(){
 else {
         println("i don't know who that us")
     }
+    var z=text("our class door",35)
+    println(z)
 }
 fun sentence(name:String,age:Int):String{
     var sentence1= "Hi, my name is $name and i am $age years old"
     return sentence1
 }
+val length=(20)
+fun text(sentence:String,length:Int):String{
+    var text="our class door is $length"
+    return text
+
+}
+
